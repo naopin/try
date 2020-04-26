866a9e8a3d33deaceea4db0dffa4173dc0037160
goro9 <sokenbiccha@icloud.com>
2020-04-26 22:23:15 +0900

    add description

--- double.js
+++ double.js
@@ -1,7 +1,14 @@
+//           0 1 2 3 4 <-index(要素番号)
 let array = [1,2,3,4,5];
+
 console.log(array)
-array = double_elm3(array)
-console.log(array)
+console.log("-----")
+
+console.log(double_elm3_2(array))
+console.log("-----")
+
+array = double_elm3_3(array)
+console.log("-----")
 
 function double_elm(array) {
     let ret = Array(array.length)
@@ -24,3 +31,24 @@ function double_elm3(array) {
     }
     return array;
 }
+
+function double_elm3_2(array) {
+    // for (let i = 0;i < array.length; i++) {
+        // 最初に見せてもらったコードのfor文展開するとこんな感じ(ifは省略)
+        // vscodeとかで開くと40行目以降が暗くなっててデッドコードになっていることが分かる
+        return array[0] * 2;
+        return array[1] * 2;
+        return array[2] * 2;
+        return array[3] * 2;
+        return array[4] * 2;
+    // 
+}
+
+function double_elm3_3(array) {
+    for (let i = 0;i < array.length; i++) {
+        console.log(array[i]);  // iには0~array.length(4)が順番に入り、配列の要素を順番に取り出せる
+    }
+    console.log(array); // 配列を丸ごと指定したい時はindexを付けない
+    return array;
+}
+
