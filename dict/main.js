0a40fb2e9849db8e51789a9c910556cb9de8aa76
goro9 <sokenbiccha@icloud.com>
2020-05-06 23:47:29 +0900

    radio button filter

--- dict/main.js
+++ dict/main.js
@@ -2,6 +2,15 @@ const todos = [];
 const inputBox = document.getElementById('input');
 const addButton = document.getElementById('add-button');
 const tBody = document.getElementById('todo-list');
+const formRadio = document.getElementById("check");
+const defRadioStatus = {
+    "": "すべて",
+    "all": "すべて",
+    "active": "作業中",
+    "complete": "完了"
+}
+formRadio.addEventListener('change', showTodos);
+
 // 追加するとtodoにテキスト格納
 addButton.addEventListener('click', () => {
     const todo = inputBox.value;
@@ -10,23 +19,32 @@ addButton.addEventListener('click', () => {
     //  配列にテキスト格納
     todos.push(getTodoTdDict(todo));
     showTodos();
-    
 });
 //配列を出力:テーブルに追加
 function showTodos() {
+    const radioStatus = formRadio.status.value;
+    // console.log(radioState);
+
     // tbodyの中身を空にする
     while (tBody.firstChild) {
         tBody.removeChild(tBody.firstChild);
     }
     todos.forEach((todoDict, index) => {
-        const tr = document.createElement('tr');
-        const tdNum = document.createElement('td');
-        tdNum.textContent = index + 1;
-        tr.appendChild(tdNum);
-        for (key in todoDict) {
-            tr.appendChild(todoDict[key]);          
+        let isVisible = false;
+        if(defRadioStatus[radioStatus] === "すべて" || defRadioStatus[radioStatus] === todoDict.progressButton.textContent) {
+            isVisible = true;
         }
-        tBody.appendChild(tr);
+
+        if(isVisible) {
+            const tr = document.createElement('tr');
+            const tdNum = document.createElement('td');
+            tdNum.textContent = index + 1;
+            tr.appendChild(tdNum);
+            for (key in todoDict) {
+                tr.appendChild(todoDict[key]);
+            }
+            tBody.appendChild(tr);
+        }
     });
 }
 // todoの連想配列を作って返す関数
@@ -42,14 +60,13 @@ function getTodoTdDict(todo) {
 
     tdDelete.appendChild(deleteButton);
     // todoDict["todo"](or todoDict.todo)でtodoの値=tdTodoにアクセスできる
-    const todoDict = {todo:tdComment, progressButton:tdProgress, deleteButton:tdDelete};
+    const todoDict = {todo: tdComment, progressButton: tdProgress, deleteButton: tdDelete};
     return todoDict;
 }
-
 function createDeleteBtn() {
     const deleteButton = document.createElement('button');
     deleteButton.textContent = '削除';
-    deleteButton.addEventListener('click', (todoDict) => {
+    deleteButton.addEventListener('click', todoDict => {
         todos.splice(todoDict, 1);
         showTodos();
     });
